0669ae476e56604d784cc141e73661e08d424694
Piotr P. Karwasz <pkarwasz-github@apache.org>
2026-05-01 00:34:09 +0200

    Rebrand headers, `pom.xml` and documentation
    While the ultimate goal is to include this project in Apache Commons or Xerces, in the ramp-up phase this project will be released under `eu.copernik` to test its utility.

--- android-tests/build.gradle.kts
+++ android-tests/build.gradle.kts
@@ -1,19 +1,8 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2026 Piotr P. Karwasz <piotr@github.copernik.eu>
+ * SPDX-License-Identifier: Apache-2.0
  */
+
 import com.android.build.api.dsl.ManagedVirtualDevice
 
 plugins {
@@ -22,10 +11,10 @@ plugins {
 }
 
 val libraryVersion = "0.1.0-SNAPSHOT"
-val libraryJar = rootProject.file("../target/commons-xml-factory-${libraryVersion}.jar")
+val libraryJar = rootProject.file("../target/copernik-xml-factory-${libraryVersion}.jar")
 
 android {
-    namespace = "org.apache.commons.xml.factory.androidtests"
+    namespace = "eu.copernik.xml.factory.androidtests"
     compileSdk = 34
 
     defaultConfig {
